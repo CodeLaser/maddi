3e557973787e9867bcbf4951f4e5ca2b4775e614
Bart Naudts <Bart.Naudts@codelaser.io>
2025-08-18 08:31:49 +0200

    tests green again in external-support, internal-util

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -3,17 +3,12 @@ plugins {
     `java-platform`
 }
 
-group = "io.codelaser"
-
-repositories{
-    mavenCentral()
-}
-
 dependencies {
     constraints {
-        api("org.junit.jupiter:junit-jupiter-api:5.13.4")
+        api("org.junit.jupiter:junit-jupiter-api:5.9.2")
+        runtime("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+
         api("org.slf4j:slf4j-api:2.0.17")
-        runtime("org.junit.jupiter:junit-jupiter-engine:5.13.4")
         runtime("ch.qos.logback:logback-classic:1.5.18")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,10 @@
 
-
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
 
 //include("aapi-archive")
 //include("aapi-parser")
