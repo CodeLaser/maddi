57a7b8411ba61f3521934600f1d60b5f8266242c
Bart Naudts <Bart.Naudts@codelaser.io>
2025-08-18 09:16:40 +0200

    added inspection, java

--- java-parser/build.gradle.kts
+++ java-parser/build.gradle.kts
@@ -5,92 +5,13 @@
  */
 
 plugins {
-    `java-library`
-    `maven-publish`
+    id("java-library-conventions")
 }
-
-group = "org.e2immu"
-
-
-repositories {
-    mavenLocal()
-    maven {
-        url = uri(project.findProperty("codeartifactPublicUri") as String)
-        credentials {
-            username = "aws"
-            password = project.findProperty("codeartifactToken") as String
-        }
-    }
-    mavenCentral()
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_24
-    targetCompatibility = JavaVersion.VERSION_24
-}
-
-val slf4jVersion = project.findProperty("slf4jVersion") as String
-val jupiterApiVersion = project.findProperty("jupiterApiVersion") as String
-val jupiterEngineVersion = project.findProperty("jupiterEngineVersion") as String
-val logbackClassicVersion = project.findProperty("logbackClassicVersion") as String
-val jetBrainsAnnotationsVersion = project.findProperty("jetBrainsAnnotationsVersion") as String
 
 dependencies {
-    api("org.e2immu:e2immu-cst-api:$version")
-    api("org.e2immu:e2immu-external-support:$version")
-    api("org.e2immu:e2immu-inspection-api:$version")
-    implementation("org.e2immu:e2immu-internal-util:$version")
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    api(project(":inspection-api"))
+    implementation(project(":internal-util"))
 
-    testImplementation("org.jetbrains:annotations:$jetBrainsAnnotationsVersion")
-    testImplementation("org.e2immu:e2immu-cst-impl:$version")
-    testImplementation("org.e2immu:e2immu-inspection-parser:$version")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$jupiterApiVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jupiterEngineVersion")
-    testRuntimeOnly("ch.qos.logback:logback-classic:$logbackClassicVersion")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-publishing {
-    repositories {
-        maven {
-            url = uri(project.findProperty("publishPublicUri") as String)
-            credentials {
-                username = project.findProperty("publishUsername") as String
-                password = project.findProperty("publishPassword") as String
-            }
-        }
-    }
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-
-            pom {
-                name = "language-java-parser of e2immu analyser"
-                description = "Static code analyser focusing on modification and immutability. " +
-                        "This module provides support for parsing Java sources using CongoCC."
-                url = "https://e2immu.org"
-                scm {
-                    url = "https://github.com/e2immu"
-                }
-                licenses {
-                    license {
-                        name = "GNU Lesser General Public License, version 3.0"
-                        url = "https://www.gnu.org/licenses/lgpl-3.0.html"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "bnaudts"
-                        name = "Bart Naudts"
-                        email = "bart.naudts@e2immu.org"
-                    }
-                }
-            }
-        }
-    }
+    testImplementation(project(":cst-impl"))
+    testImplementation(project(":inspection-parser"))
 }
