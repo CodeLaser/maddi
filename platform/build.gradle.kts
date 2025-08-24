d6cedf233ff904a103532db7298ab4093df4bd05
Bart Naudts <Bart.Naudts@codelaser.io>
2025-08-24 08:16:55 +0200

    do not need a constraint on the jupiter-engine; its version will come from the bom

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -9,8 +9,7 @@ dependencies {
         api("org.jgrapht:jgrapht-io:1.5.2")
 
         api("org.junit.jupiter:junit-jupiter-api:5.9.2")
-        runtime("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-
+        
         api("org.slf4j:slf4j-api:2.0.17")
         api("ch.qos.logback:logback-classic:1.5.18")
 
