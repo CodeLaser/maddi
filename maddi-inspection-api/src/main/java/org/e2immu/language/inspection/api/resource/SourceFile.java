53f38cbfd8fe9d0e722f08b2301809cefe2d0e7b
Bart Naudts <Bart.Naudts@codelaser.io>
2025-09-28 12:57:20 +0200

    intermediate commit, inspection/integration does not yet run; problems with fqn from path

--- maddi-inspection-api/src/main/java/org/e2immu/language/inspection/api/resource/SourceFile.java
+++ maddi-inspection-api/src/main/java/org/e2immu/language/inspection/api/resource/SourceFile.java
@@ -2,9 +2,12 @@ package org.e2immu.language.inspection.api.resource;
 
 import org.e2immu.language.cst.api.element.FingerPrint;
 import org.e2immu.language.cst.api.element.SourceSet;
+import org.e2immu.util.internal.util.StringUtil;
 
 import java.net.URI;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public record SourceFile(String path, URI uri, SourceSet sourceSet, FingerPrint fingerPrint) {
 
@@ -12,6 +15,19 @@ public record SourceFile(String path, URI uri, SourceSet sourceSet, FingerPrint 
     public boolean equals(Object o) {
         if (!(o instanceof SourceFile that)) return false;
         return Objects.equals(uri(), that.uri()) && Objects.equals(sourceSet(), that.sourceSet());
+    }
+
+    private static final Pattern PATTERN = Pattern.compile(".+(!/|:)(.+)\\.class");
+
+    public String fullyQualifiedName() {
+        String uri  = uri().toString();
+        // go bock until !/ or :
+        Matcher m = PATTERN.matcher(uri);
+        if(m.matches()) {
+            String pathWithoutDotClass = m.group(2);
+            return StringUtil.replaceSlashDollar(pathWithoutDotClass);
+        }
+        throw new UnsupportedOperationException();
     }
 
     @Override
