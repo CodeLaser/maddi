9857b102a7c883a4b8b82ade89adc86489422857
Bart Naudts <Bart.Naudts@codelaser.io>
2025-10-06 13:32:40 +0200

    test now fails

--- maddi-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/method/TestMethodCall13.java
+++ maddi-inspection-integration/src/test/java/org/e2immu/language/inspection/integration/java/method/TestMethodCall13.java
@@ -1,5 +1,6 @@
 package org.e2immu.language.inspection.integration.java.method;
 
+import org.e2immu.language.cst.api.expression.Expression;
 import org.e2immu.language.cst.api.expression.Lambda;
 import org.e2immu.language.cst.api.info.MethodInfo;
 import org.e2immu.language.cst.api.info.TypeInfo;
@@ -13,35 +14,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestMethodCall13 extends CommonTest {
 
- //   @Language("java")
+    @Language("java")
     private static final String INPUT1 = """
-         return clinicalDataCountItems.stream()
-               // Exclude NA category
-               .map(
-                   clinicalDataCountItem -> {
-                     List<ClinicalDataCount> filteredClinicalDataCount =
-                         clinicalDataCountItem.getCounts().stream()
-                             .filter(
-                                 clinicalDataCount -> {
-                                   if (ComparisonCategoricalNaValuesString != null) {
-                                     String[] ComparisonCategoricalNaValues =
-                                         ComparisonCategoricalNaValuesString.split("\\\\|");
-                                     for (String naValue : ComparisonCategoricalNaValues) {
-                                       if (clinicalDataCount.getValue().equalsIgnoreCase(naValue)) {
-                                         return false;
-                                       }
-                                     }
-                                   }
-                                   return true;
-                                 })
-                             .collect(Collectors.toList());
-                     clinicalDataCountItem.setCounts(filteredClinicalDataCount);
-                     return clinicalDataCountItem;
-                   })
-               .collect(
-                   Collectors.toMap(
-                       clinicalDataCountItem -> clinicalDataCountItem.getAttributeId(),
-                       clinicalDataCountItem -> clinicalDataCountItem));
+         package a.b;
+         import java.util.List;
+         import java.util.Map;import java.util.stream.Collectors;
+         
+         class X {
+             interface ClinicalDataCount { 
+                 boolean accept();
+             }
+             interface ClinicalDataCountItem {
+                  List<ClinicalDataCount> getCounts();
+                  void setCounts(List<ClinicalDataCount> list);
+                  String getAttributeId();
+              }
+
+             public Map<String, ClinicalDataCountItem> method(List<ClinicalDataCountItem> clinicalDataCountItems) {
+                 return clinicalDataCountItems.stream()
+                       // Exclude NA category
+                       .map(
+                           clinicalDataCountItem -> {
+                             List<ClinicalDataCount> filteredClinicalDataCount =
+                                 clinicalDataCountItem.getCounts().stream()
+                                     .filter(
+                                         clinicalDataCount -> {
+                                           if(clinicalDataCount.accept()) {
+                                               return true;
+                                           }
+                                           return false;
+                                         })
+                                     .collect(Collectors.toList());
+                             clinicalDataCountItem.setCounts(filteredClinicalDataCount);
+                             return clinicalDataCountItem;
+                           })
+                       .collect(
+                           Collectors.toMap(
+                               clinicalDataCountItem -> clinicalDataCountItem.getAttributeId(),
+                               clinicalDataCountItem -> clinicalDataCountItem));
+                }
+            }
             """;
 
     /*
@@ -51,11 +63,12 @@ public class TestMethodCall13 extends CommonTest {
     R clearly can become a LinkedHashSet
     evaluation of the lambda must occur with R=LinkedHashSet, as must evaluation of X::combiner
      */
-    @Disabled
     @Test
     public void test1() {
         TypeInfo typeInfo = javaInspector.parse(INPUT1);
-
+        MethodInfo method = typeInfo.findUniqueMethod("method", 1);
+        Expression expression = method.methodBody().statements().getFirst().expression();
+        assertEquals("", expression.toString());
     }
 
 }
