d4600d61f4f0acb2966aed37c1c7bcbcfe4774c1
Bart Naudts <Bart.Naudts@codelaser.io>
2025-12-04 09:14:54 +0100

    first test, start to implement, 3

--- maddi-modification-link/src/main/java/org/e2immu/analyzer/modification/link/Links.java
+++ maddi-modification-link/src/main/java/org/e2immu/analyzer/modification/link/Links.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public interface Links extends Iterable<Link> {
 
+    default Link first() {
+        return links().isEmpty() ? null : links().getFirst();
+    }
+
     default boolean isEmpty() {
         return links().isEmpty();
     }
--- maddi-modification-link/src/main/java/org/e2immu/analyzer/modification/link/impl/Expand.java
+++ maddi-modification-link/src/main/java/org/e2immu/analyzer/modification/link/impl/Expand.java
@@ -1,11 +1,33 @@
 package org.e2immu.analyzer.modification.link.impl;
 
+import org.e2immu.analyzer.modification.link.Link;
 import org.e2immu.analyzer.modification.link.LinkedVariables;
 import org.e2immu.analyzer.modification.link.Links;
+import org.e2immu.analyzer.modification.prepwork.variable.ReturnVariable;
 import org.e2immu.analyzer.modification.prepwork.variable.VariableData;
+import org.e2immu.language.cst.api.variable.LocalVariable;
+import org.e2immu.language.cst.api.variable.Variable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Expand {
-    public static Links expand(Links links, LinkedVariables extra, VariableData vd) {
+
+    public static Links connect(LocalVariable lv, Links links) {
         return links; // FIXME more!
+    }
+
+    public static Links expandReturnValue(ReturnVariable returnVariable, Links links, LinkedVariables extra, VariableData vd) {
+        List<Link> list = new ArrayList<>(links.links());
+        Link link = links.first();
+        if (link != null) {
+            list.set(0, new LinkImpl(returnVariable, link.linkNature(), link.to()));
+        }
+        list.removeIf(l -> l.from() != null && containsLocal(l.from()) || containsLocal(l.to()));
+        return new LinksImpl(list);
+    }
+
+    private static boolean containsLocal(Variable variable) {
+        return variable.variableStreamDescend().anyMatch(v -> v instanceof LocalVariable);
     }
 }
