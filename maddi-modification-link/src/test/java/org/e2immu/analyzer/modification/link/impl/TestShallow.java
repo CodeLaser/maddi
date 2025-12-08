4634f44cbc218d20eb803b6e1acc95a884dfac89
Bart Naudts <Bart.Naudts@codelaser.io>
2025-12-08 07:35:24 +0100

    started testOptional, more in multiplicity 1 test

--- maddi-modification-link/src/test/java/org/e2immu/analyzer/modification/link/impl/TestShallow.java
+++ maddi-modification-link/src/test/java/org/e2immu/analyzer/modification/link/impl/TestShallow.java
@@ -13,29 +13,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestShallow extends CommonTest {
-
-    @Test
-    public void test() {
-        LinkComputer lc = new LinkComputerImpl(javaInspector);
-        TypeInfo list = javaInspector.compiledTypesManager().getOrLoad(List.class);
-        MethodInfo get = list.findUniqueMethod("get", 1);
-        lc.doMethod(get);
-    }
-
 
     @Language("java")
     private static final String INPUT1 = """
             package a.b;
             public interface X<T> {
                 T get();
+                void set(T t);
+                String label(int k);
             }
             """;
 
-    @DisplayName("Analyze 'get', map access, manually inserting links for Map.get(K)")
+    @DisplayName("Analyze 'get/set', multiplicity 1")
     @Test
     public void test1() {
         TypeInfo X = javaInspector.parse(INPUT1);
@@ -44,9 +38,57 @@ public class TestShallow extends CommonTest {
         VirtualFieldComputer vfc = new VirtualFieldComputer(javaInspector);
         VirtualFields vfX = vfc.compute(X);
         assertEquals("$m - T t", vfX.toString());
+
         LinkComputer linkComputer = new LinkComputerImpl(javaInspector);
+
         MethodInfo get = X.findUniqueMethod("get", 0);
-        MethodLinkedVariables mlv = linkComputer.doMethod(get);
-        assertEquals("[] --> get==this.t", mlv.toString());
+        MethodLinkedVariables mlvGet = linkComputer.doMethod(get);
+        assertEquals("[] --> get==this.t", mlvGet.toString());
+
+        MethodInfo set = X.findUniqueMethod("set", 1);
+        MethodLinkedVariables mlvSet = linkComputer.doMethod(set);
+        assertEquals("[0:t==this.t] --> -", mlvSet.toString());
+
+        MethodInfo label = X.findUniqueMethod("label", 1);
+        MethodLinkedVariables mlvLabel = linkComputer.doMethod(label);
+        assertEquals("[-] --> -", mlvLabel.toString());
     }
+
+
+    @DisplayName("Analyze 'Optional', multiplicity 1")
+    @Test
+    public void test2() {
+        TypeInfo optional = javaInspector.compiledTypesManager().getOrLoad(Optional.class);
+        PrepAnalyzer analyzer = new PrepAnalyzer(runtime, new PrepAnalyzer.Options.Builder().build());
+        analyzer.doPrimaryType(optional);
+        VirtualFieldComputer vfc = new VirtualFieldComputer(javaInspector);
+        VirtualFields vfX = vfc.compute(optional);
+        assertEquals("/ - T t", vfX.toString());
+
+        LinkComputer linkComputer = new LinkComputerImpl(javaInspector);
+
+        MethodInfo get = optional.findUniqueMethod("get", 0);
+        MethodLinkedVariables mlvGet = linkComputer.doMethod(get);
+        assertEquals("[] --> get==this.t", mlvGet.toString());
+
+        MethodInfo set = optional.findUniqueMethod("orElse", 1);
+        MethodLinkedVariables mlvSet = linkComputer.doMethod(set);
+        assertEquals("[0:other==orElse] --> orElse==this.t,orElse==0:other", mlvSet.toString());
+
+        MethodInfo label = optional.findUniqueMethod("stream", 0);
+        MethodLinkedVariables mlvLabel = linkComputer.doMethod(label);
+        assertEquals("[] --> stream>this.t", mlvLabel.toString());
+    }
+
+    @DisplayName("Analyze 'List', multiplicity 2")
+    @Test
+    public void test3() {
+        LinkComputer linkComputer = new LinkComputerImpl(javaInspector);
+        TypeInfo list = javaInspector.compiledTypesManager().getOrLoad(List.class);
+        MethodInfo get = list.findUniqueMethod("get", 1);
+        MethodLinkedVariables mlvGet = linkComputer.doMethod(get);
+        assertEquals("[] --> get<this.ts", mlvGet.toString());
+    }
+
+
 }
