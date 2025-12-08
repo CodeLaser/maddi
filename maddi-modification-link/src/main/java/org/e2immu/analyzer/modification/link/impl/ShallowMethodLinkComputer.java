627b28fd7caa9646cdb113dde9cfb9f20e47de0c
Bart Naudts <Bart.Naudts@codelaser.io>
2025-12-08 10:36:20 +0100

    first param -> object, factory method param -> rv

--- maddi-modification-link/src/main/java/org/e2immu/analyzer/modification/link/impl/ShallowMethodLinkComputer.java
+++ maddi-modification-link/src/main/java/org/e2immu/analyzer/modification/link/impl/ShallowMethodLinkComputer.java
@@ -11,6 +11,7 @@ import org.e2immu.language.cst.api.info.*;
 import org.e2immu.language.cst.api.runtime.Runtime;
 import org.e2immu.language.cst.api.type.ParameterizedType;
 import org.e2immu.language.cst.api.variable.FieldReference;
+import org.e2immu.language.cst.api.variable.Variable;
 import org.e2immu.language.cst.impl.analysis.PropertyImpl;
 import org.e2immu.language.cst.impl.analysis.ValueImpl;
 import org.e2immu.language.inspection.api.parser.GenericsHelper;
@@ -36,42 +37,8 @@ public record ShallowMethodLinkComputer(Runtime runtime, VirtualFieldComputer vi
         ReturnVariableImpl rv = new ReturnVariableImpl(methodInfo);
         Links.Builder ofReturnValue = new LinksImpl.Builder(rv);
         Set<TypeParameter> typeParametersVf = correspondingTypeParameters(methodInfo.typeInfo(), vf.hiddenContent());
-        if (methodInfo.hasReturnValue() && vf.hiddenContent() != null) {
-
-            Value.Independent independent = methodInfo.analysis().getOrDefault(PropertyImpl.INDEPENDENT_METHOD,
-                    ValueImpl.IndependentImpl.DEPENDENT);
-            ParameterizedType returnType = methodInfo.returnType();
-            if (!independent.isIndependent()) {
-                int arraysVF = vf.hiddenContent().type().arrays();
-                if (returnType.typeParameter() != null && typeParametersVf.contains(returnType.typeParameter())) {
-                    assert independent.isIndependentHc() : "A type parameter cannot be dependent";
-                    assert returnType.typeParameter().typeBounds().isEmpty() : """
-                            cannot deal with type bounds at the moment; obviously, if a type bound is mutable,
-                            the type parameter can be dependent""";
-                    int arrays = returnType.arrays();
-                    if (arrays == arraysVF) {
-                        if (vf.hiddenContent().type().typeParameter() != null) {
-                            // must be the same; same element
-                            ofReturnValue.add(LinkNature.IS_IDENTICAL_TO, hiddenContentFr);
-                        }
-                    } else if (arrays < arraysVF) {
-                        if (vf.hiddenContent().type().typeParameter() != null) {
-                            // one element out of an array
-                            ofReturnValue.add(LinkNature.IS_ELEMENT_OF, hiddenContentFr);
-                        }
-                    }
-                } else if (returnType.typeInfo() != null) {
-                    Set<TypeParameter> typeParametersReturnType = returnType.extractTypeParameters();
-                    if (typeParametersReturnType.equals(typeParametersVf)) {
-                        int multiplicity = virtualFieldComputer.computeMultiplicity(returnType);
-                        if (multiplicity == 0 && arraysVF == 0) {
-                            ofReturnValue.add(LinkNature.CONTAINS, hiddenContentFr);
-                        } else if (multiplicity - 1 == arraysVF) {
-                            ofReturnValue.add(LinkNature.INTERSECTION_NOT_EMPTY, hiddenContentFr);
-                        }
-                    }
-                }
-            }
+        if (methodInfo.hasReturnValue() && vf.hiddenContent() != null && !methodInfo.isStatic()) {
+            instanceMethodRvToObject(methodInfo, vf, typeParametersVf, ofReturnValue, hiddenContentFr);
         }
 
         List<Links> ofParameters = new ArrayList<>(methodInfo.parameters().size());
@@ -83,33 +50,84 @@ public record ShallowMethodLinkComputer(Runtime runtime, VirtualFieldComputer vi
                     ValueImpl.IndependentImpl.DEPENDENT);
             Map<Integer, Integer> dependencies = independent.linkToParametersReturnValue();
             if (!dependencies.isEmpty()) {
-                LOGGER.debug("Parameter {} has dependencies on {}", pi, dependencies);
-                int linkLevel = dependencies.getOrDefault(-1, -1);
-                // a dependence from the parameter into the return variable; we'll add it to the return variable
-                // linkLevel 1 == independent HC
-                if (type.typeParameter() != null && linkLevel == 1) {
-                    int arrays = type.arrays();
-                    int arraysVF = rv.parameterizedType().arrays();
-                    if (arrays == arraysVF) {
-                        if (rv.parameterizedType().typeParameter() != null) {
-                            // must be the same; same element
-                            ofReturnValue.add(LinkNature.IS_IDENTICAL_TO, pi);
-                        }
-                    }
-                }
-            } else if (type.typeParameter() != null && vf.hiddenContent() != null && independent.isIndependentHc()) {
-                int arrays = type.arrays();
-                int arraysVF = vf.hiddenContent().type().arrays();
-                if (arrays == arraysVF) {
-                    if (vf.hiddenContent().type().typeParameter() != null) {
-                        // must be the same; same element
-                        builder.add(LinkNature.IS_IDENTICAL_TO, hiddenContentFr);
-                    }
+                explicitTransferFromParametersToRv(pi, dependencies, type, vf, ofReturnValue);
+            } else if (!independent.isIndependent()) {
+                if (methodInfo.isFactoryMethod()) {
+                    transfer(type, vf, ofReturnValue, pi, true);
+                } else {
+                    transfer(type, vf, builder, hiddenContentFr, false);
                 }
             }
             ofParameters.add(builder.build());
         }
         return new MethodLinkedVariablesImpl(ofReturnValue.build(), ofParameters);
+    }
+
+    private static void transfer(ParameterizedType type,
+                                 VirtualFields vf,
+                                 Links.Builder builder,
+                                 Variable hiddenContentFr,
+                                 boolean reverse) {
+        if (type.typeParameter() != null && vf.hiddenContent() != null) {
+            int arrays = type.arrays();
+            int arraysVF = vf.hiddenContent().type().arrays();
+            if (arrays == arraysVF) {
+                if (vf.hiddenContent().type().typeParameter() != null) {
+                    // must be the same; same element
+                    builder.add(LinkNature.IS_IDENTICAL_TO, hiddenContentFr);
+                }
+            } else if (arrays < arraysVF) {
+                if (vf.hiddenContent().type().typeParameter() != null) {
+                    // one element out of an array
+                    LinkNature linkNature = reverse ? LinkNature.CONTAINS : LinkNature.IS_ELEMENT_OF;
+                    builder.add(linkNature, hiddenContentFr);
+                }
+            }
+        }
+    }
+
+    private static void explicitTransferFromParametersToRv(ParameterInfo pi,
+                                                           Map<Integer, Integer> dependencies,
+                                                           ParameterizedType type,
+                                                           VirtualFields vf,
+                                                           Links.Builder ofReturnValue) {
+        LOGGER.debug("Parameter {} has dependencies on {}", pi, dependencies);
+        int linkLevel = dependencies.getOrDefault(-1, -1);
+        // a dependence from the parameter into the return variable; we'll add it to the return variable
+        // linkLevel 1 == independent HC
+        if (type.typeParameter() != null && linkLevel == 1) {
+            transfer(pi.parameterizedType(), vf, ofReturnValue, pi, true);
+        }
+    }
+
+    private void instanceMethodRvToObject(MethodInfo methodInfo,
+                                          VirtualFields vf,
+                                          Set<TypeParameter> typeParametersVf,
+                                          Links.Builder ofReturnValue,
+                                          FieldReference hiddenContentFr) {
+        Value.Independent independent = methodInfo.analysis().getOrDefault(PropertyImpl.INDEPENDENT_METHOD,
+                ValueImpl.IndependentImpl.DEPENDENT);
+        ParameterizedType returnType = methodInfo.returnType();
+        if (!independent.isIndependent()) {
+            int arraysVF = vf.hiddenContent().type().arrays();
+            if (returnType.typeParameter() != null && typeParametersVf.contains(returnType.typeParameter())) {
+                assert independent.isIndependentHc() : "A type parameter cannot be dependent";
+                assert returnType.typeParameter().typeBounds().isEmpty() : """
+                        cannot deal with type bounds at the moment; obviously, if a type bound is mutable,
+                        the type parameter can be dependent""";
+                transfer(returnType, vf, ofReturnValue, hiddenContentFr, false);
+            } else if (returnType.typeInfo() != null) {
+                Set<TypeParameter> typeParametersReturnType = returnType.extractTypeParameters();
+                if (typeParametersReturnType.equals(typeParametersVf)) {
+                    int multiplicity = virtualFieldComputer.computeMultiplicity(returnType);
+                    if (multiplicity == 0 && arraysVF == 0) {
+                        ofReturnValue.add(LinkNature.CONTAINS, hiddenContentFr);
+                    } else if (multiplicity - 1 == arraysVF) {
+                        ofReturnValue.add(LinkNature.INTERSECTION_NOT_EMPTY, hiddenContentFr);
+                    }
+                }
+            }
+        }
     }
 
     /*
@@ -126,15 +144,18 @@ public record ShallowMethodLinkComputer(Runtime runtime, VirtualFieldComputer vi
             return hiddenContent.type().extractTypeParameters();
         }
         Stream<ParameterizedType> parentStream = Stream.ofNullable(
-                descendantType.parentClass() == null || descendantType.parentClass().isJavaLangObject() ? null : descendantType.parentClass());
-        Stream<ParameterizedType> superStream = Stream.concat(parentStream, descendantType.interfacesImplemented().stream());
+                descendantType.parentClass() == null || descendantType.parentClass().isJavaLangObject()
+                        ? null : descendantType.parentClass());
+        Stream<ParameterizedType> superStream = Stream.concat(parentStream,
+                descendantType.interfacesImplemented().stream());
         GenericsHelper genericsHelper = new GenericsHelperImpl(runtime);
         return superStream.flatMap(superType -> {
             Set<TypeParameter> fromSuper = correspondingTypeParameters(superType.typeInfo(), hiddenContent);
             var map = genericsHelper.mapInTermsOfParametersOfSubType(descendantType, superType);
             return map == null ? Stream.of()
                     : map.entrySet().stream()
-                    .filter(e -> e.getValue().typeParameter() != null && fromSuper.contains(e.getValue().typeParameter()))
+                    .filter(e -> e.getValue().typeParameter() != null
+                                 && fromSuper.contains(e.getValue().typeParameter()))
                     .map(e -> (TypeParameter) e.getKey());
         }).collect(Collectors.toUnmodifiableSet());
     }
