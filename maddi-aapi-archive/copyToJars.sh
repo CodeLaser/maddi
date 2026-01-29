2f46f7d663badc8838063bb1a885b2e6be432480
Bart Naudts <Bart.Naudts@codelaser.io>
2026-01-29 13:59:24 +0100

    25.0.2, now better

--- maddi-aapi-archive/copyToJars.sh
+++ maddi-aapi-archive/copyToJars.sh
@@ -16,9 +16,9 @@ cd src/main/resources/org/e2immu/analyzer/aapi/archive/analyzedPackageFiles/jdk/
 cd openjdk-24.0.2
 jar cf ../openjdk-24.0.2.jar *.json
 cd ../openjdk-21.0.9/
-jar cf ../openjdk-21.0.9.jar *.json
+jar cf ../openjdk-21.0.10.jar *.json
 cd ../openjdk-25.0.1/
-jar cf ../openjdk-25.0.1.jar *.json
+jar cf ../openjdk-25.0.2.jar *.json
 cd ../../libs/
 jar cf ../libs.jar */*.json
 
