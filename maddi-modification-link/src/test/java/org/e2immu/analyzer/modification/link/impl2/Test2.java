bdb906894ee07ce91969b50eaf822fcafd258aa8
Bart Naudts <Bart.Naudts@codelaser.io>
2026-01-30 12:05:19 +0100

    removing cyclic links, in-between links §kv.§m -> §m

---
+++ maddi-modification-link/src/test/java/org/e2immu/analyzer/modification/link/impl2/Test2.java
@@ -0,0 +1,47 @@
+package org.e2immu.analyzer.modification.link.impl2;
+
+import org.e2immu.analyzer.modification.link.CommonTest;
+import org.e2immu.analyzer.modification.link.LinkComputer;
+import org.e2immu.analyzer.modification.link.impl.LinkComputerImpl;
+import org.e2immu.analyzer.modification.prepwork.PrepAnalyzer;
+import org.e2immu.language.cst.api.info.TypeInfo;
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Test2 extends CommonTest {
+
+    @Language("java")
+    private static final String INPUT1 = """
+            package a.b;
+
+            class X {
+                interface Runtime { }
+                static class PA {
+                    MA methodAnalyzer;
+                    PA(Runtime runtime) {
+                        methodAnalyzer = new MA(runtime, this);
+                    }
+                }
+                static class MA {
+                    PA prepAnalyzer;
+                    Runtime runtime;
+                    MA(Runtime runtime, PA prepAnalyzer) {
+                        this.runtime = runtime;
+                        this.prepAnalyzer = prepAnalyzer;
+                    }
+                }
+            }
+            """;
+
+    @DisplayName("cycle protection")
+    @Test
+    public void test1() {
+        TypeInfo C = javaInspector.parse(INPUT1);
+        PrepAnalyzer analyzer = new PrepAnalyzer(runtime, new PrepAnalyzer.Options.Builder().build());
+        analyzer.doPrimaryType(C);
+        LinkComputer tlc = new LinkComputerImpl(javaInspector,
+                new LinkComputer.Options.Builder().setRecurse(true).setCheckDuplicateNames(false).build());
+        tlc.doPrimaryType(C);
+    }
+}
