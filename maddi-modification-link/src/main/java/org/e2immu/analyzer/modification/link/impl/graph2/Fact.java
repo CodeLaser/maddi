5717b119fabf4dad25580709a06817d67cd5d85e
Bart Naudts <Bart.Naudts@codelaser.io>
2026-04-07 09:16:50 +0200

    ran first tests, improved printing; 177 green, 86 red

--- maddi-modification-link/src/main/java/org/e2immu/analyzer/modification/link/impl/graph2/Fact.java
+++ maddi-modification-link/src/main/java/org/e2immu/analyzer/modification/link/impl/graph2/Fact.java
@@ -2,9 +2,15 @@ package org.e2immu.analyzer.modification.link.impl.graph2;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Function;
+
 public record Fact<V, L>(V source, V target, L label) {
     public Fact {
         assert !source.equals(target);
+    }
+
+    public String print(Function<V, String> vertexPrinter) {
+        return vertexPrinter.apply(source) + " " + label + " " + vertexPrinter.apply(target);
     }
 
     @Override
--- maddi-modification-link/src/main/java/org/e2immu/analyzer/modification/link/impl/graph2/Witness.java
+++ maddi-modification-link/src/main/java/org/e2immu/analyzer/modification/link/impl/graph2/Witness.java
@@ -2,14 +2,24 @@ package org.e2immu.analyzer.modification.link.impl.graph2;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Function;
+
 // explains how a certain fact was derived
 public sealed interface Witness<V, L>
         permits Witness.DirectWitness, Witness.CompositeWitness {
+
+    String print(Function<V, String> vertexPrinter);
 
     record DirectWitness<V, L>(V from, V to, L label, String statementIndex) implements Witness<V, L> {
         @Override
         public @NotNull String toString() {
             return statementIndex + "(" + from + " " + label + " " + to + ")";
+        }
+
+        @Override
+        public String print(Function<V, String> vertexPrinter) {
+            return statementIndex + "(" + vertexPrinter.apply(from) + " " + label + " "
+                   + vertexPrinter.apply(to) + ")";
         }
     }
 
@@ -18,6 +28,11 @@ public sealed interface Witness<V, L>
         @Override
         public @NotNull String toString() {
             return "[" + left + ", " + right + "]";
+        }
+
+        @Override
+        public String print(Function<V, String> vertexPrinter) {
+            return "[" + left.print(vertexPrinter) + ", " + right.print(vertexPrinter) + "]";
         }
     }
 }
