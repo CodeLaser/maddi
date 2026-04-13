fa38be8d74727850ed00238a3efe4092521bbec3
Bart Naudts <Bart.Naudts@codelaser.io>
2026-04-13 07:09:27 +0200

    moved TestReassignment into TestSimpleSharedVariable

--- maddi-modification-link/src/test/java/org/e2immu/analyzer/modification/link/impl/basics/TestSimpleSharedVariable.java
+++ maddi-modification-link/src/test/java/org/e2immu/analyzer/modification/link/impl/basics/TestSimpleSharedVariable.java
@@ -6,6 +6,9 @@ import org.e2immu.analyzer.modification.link.impl.LinkComputerImpl;
 import org.e2immu.analyzer.modification.link.impl.linkgraph.Graph;
 import org.e2immu.analyzer.modification.prepwork.PrepAnalyzer;
 import org.e2immu.analyzer.modification.prepwork.variable.MethodLinkedVariables;
+import org.e2immu.analyzer.modification.prepwork.variable.VariableData;
+import org.e2immu.analyzer.modification.prepwork.variable.VariableInfo;
+import org.e2immu.analyzer.modification.prepwork.variable.impl.VariableDataImpl;
 import org.e2immu.language.cst.api.info.MethodInfo;
 import org.e2immu.language.cst.api.info.TypeInfo;
 import org.intellij.lang.annotations.Language;
@@ -37,7 +40,7 @@ public class TestSimpleSharedVariable extends CommonTest {
         PrepAnalyzer analyzer = new PrepAnalyzer(runtime, new PrepAnalyzer.Options.Builder().build());
         analyzer.doPrimaryType(X);
         LinkComputer tlc = new LinkComputerImpl(javaInspector, (statementIndex, graph) -> {
-            if("1".equals(statementIndex)) {
+            if ("1".equals(statementIndex)) {
                 Assertions.assertEquals("""
                         return method ∈ $__sv_copy.§$s   1(return method ∈ $__sv_copy.§$s)
                         $__sv_copy.§$s ∋ return method   1($__sv_copy.§$s ∋ return method)
@@ -69,13 +72,13 @@ public class TestSimpleSharedVariable extends CommonTest {
         PrepAnalyzer analyzer = new PrepAnalyzer(runtime, new PrepAnalyzer.Options.Builder().build());
         analyzer.doPrimaryType(X);
         LinkComputer tlc = new LinkComputerImpl(javaInspector, (statementIndex, graph) -> {
-            if("1".equals(statementIndex)) {
+            if ("1".equals(statementIndex)) {
                 Assertions.assertEquals("""
                         first ∈ $__sv_copy.§$s   1(first ∈ $__sv_copy.§$s)
                         $__sv_copy.§$s ∋ first   1($__sv_copy.§$s ∋ first)
                         """, graph.printClosure());
             }
-            if("2".equals(statementIndex)) {
+            if ("2".equals(statementIndex)) {
                 Assertions.assertEquals("""
                         $__sv_return method ∈ $__sv_copy.§$s   2($__sv_return method ∈ $__sv_copy.§$s)
                         $__sv_copy.§$s ∋ $__sv_return method   2($__sv_copy.§$s ∋ $__sv_return method)
@@ -86,4 +89,118 @@ public class TestSimpleSharedVariable extends CommonTest {
         MethodLinkedVariables mlv = method.analysis().getOrCreate(METHOD_LINKS, () -> tlc.doMethod(method));
         assertEquals("[-] --> method∈0:in.§$s", mlv.toString());
     }
+
+
+    @Language("java")
+    private static final String INPUT3 = """
+            package a.b;
+            import java.util.ArrayList;
+            import java.util.List;
+            class X {
+                String method(List<String> in) {
+                    List<String> copy = new ArrayList<>(in);
+                    String first = copy.getFirst();
+                    return first;
+                }
+            }
+            """;
+
+    @DisplayName("add new ArrayList")
+    @Test
+    public void test3() {
+        TypeInfo X = javaInspector.parse(INPUT3);
+        PrepAnalyzer analyzer = new PrepAnalyzer(runtime, new PrepAnalyzer.Options.Builder().build());
+        analyzer.doPrimaryType(X);
+        LinkComputer tlc = new LinkComputerImpl(javaInspector, (statementIndex, graph) -> {
+            if ("0".equals(statementIndex)) {
+                assertEquals("""
+                        0:in.§$s ⊇ copy.§$s   0(0:in.§$s ⊇ copy.§$s)
+                        copy.§$s ⊆ 0:in.§$s   0(copy.§$s ⊆ 0:in.§$s)
+                        """, graph.printClosure());
+            }
+            // (3 choose 2) = 3 combinations; always x2 because of symmetry
+            if ("1".equals(statementIndex)) {
+                assertEquals("""
+                        first ∈ 0:in.§$s   *[first ∈ copy.§$s, copy.§$s ⊆ 0:in.§$s]
+                        first ∈ copy.§$s   1(first ∈ copy.§$s)
+                        0:in.§$s ∋ first   *[0:in.§$s ⊇ copy.§$s, copy.§$s ∋ first]
+                        0:in.§$s ⊇ copy.§$s   0(0:in.§$s ⊇ copy.§$s)
+                        copy.§$s ∋ first   1(copy.§$s ∋ first)
+                        copy.§$s ⊆ 0:in.§$s   0(copy.§$s ⊆ 0:in.§$s)
+                        """, graph.printClosure());
+            }
+            // now comes a new variable, but because it goes into an equivalence group, it remains (3 choose 2)
+            // the alternative was 6 instead of 2 related to first+return
+            if ("2".equals(statementIndex)) {
+                assertEquals("""
+                        $__sv_return method ∈ 0:in.§$s   *[$__sv_return method ∈ copy.§$s, copy.§$s ⊆ 0:in.§$s]
+                        $__sv_return method ∈ copy.§$s   2($__sv_return method ∈ copy.§$s)
+                        0:in.§$s ∋ $__sv_return method   *[0:in.§$s ⊇ copy.§$s, copy.§$s ∋ $__sv_return method]
+                        0:in.§$s ⊇ copy.§$s   0(0:in.§$s ⊇ copy.§$s)
+                        copy.§$s ∋ $__sv_return method   2(copy.§$s ∋ $__sv_return method)
+                        copy.§$s ⊆ 0:in.§$s   0(copy.§$s ⊆ 0:in.§$s)
+                        """, graph.printClosure());
+            }
+        });
+        MethodInfo method = X.findUniqueMethod("method", 1);
+        MethodLinkedVariables mlv = method.analysis().getOrCreate(METHOD_LINKS, () -> tlc.doMethod(method));
+        assertEquals("[-] --> method∈0:in.§$s", mlv.toString());
+    }
+
+
+    @Language("java")
+    private static final String INPUT4 = """
+            package a.b;
+            import java.util.ArrayList;import java.util.Collections;
+            import java.util.List;
+            class X {
+                String field;
+                String second;
+                List<String> method(List<String> in) {
+                    List<String> copy = in;
+                    copy.add(field);
+                    copy = new ArrayList<>();
+                    copy.add(second);
+                    return copy;
+                }
+            }
+            """;
+
+    @DisplayName("Simple reassignment")
+    @Test
+    public void test4() {
+        TypeInfo X = javaInspector.parse(INPUT4);
+        PrepAnalyzer analyzer = new PrepAnalyzer(runtime, new PrepAnalyzer.Options.Builder().build());
+        analyzer.doPrimaryType(X);
+
+        LinkComputerImpl linkComputer = new LinkComputerImpl(javaInspector, new LinkComputerImpl.TestVisitor() {
+            @Override
+            public void visit(String statementIndex, Graph graph) {
+                if ("1".equals(statementIndex)) {
+                    assertEquals("""
+                            this.field ∈ $__sv_copy.§$s   1(this.field ∈ $__sv_copy.§$s)
+                            $__sv_copy.§$s ∋ this.field   1($__sv_copy.§$s ∋ this.field)
+                            """, graph.printClosure());
+                }
+            }
+        });
+        MethodInfo method = X.findUniqueMethod("method", 1);
+        MethodLinkedVariables mlvListAdd = method.analysis().getOrCreate(METHOD_LINKS,
+                () -> linkComputer.doMethod(method));
+
+        VariableData vd2 = VariableDataImpl.of(method.methodBody().statements().get(2));
+        VariableInfo field2 = vd2.variableInfo("a.b.X.field");
+        assertEquals("this.field∈0:in.§$s", field2.linkedVariables().toString());
+        VariableInfo copy2 = vd2.variableInfo("copy");
+        // '-' because we're not tracking the intermediary variable
+        assertEquals("-", copy2.linkedVariables().toString());
+
+        VariableData vd3 = VariableDataImpl.of(method.methodBody().statements().get(3));
+        VariableInfo copy3 = vd3.variableInfo("copy");
+        assertEquals("copy.§$s∋this.second", copy3.linkedVariables().toString());
+
+        // important that this.second not part of 0:in
+        assertEquals("[0:in*.§$s∋this.field] --> method.§$s∋this.second", mlvListAdd.toString());
+    }
+
 }
