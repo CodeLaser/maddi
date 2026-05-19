35ed46ddb0f936d854f48e801b4bbdf70c998f2d
Bart Naudts <Bart.Naudts@codelaser.io>
2026-05-19 07:08:44 +0200

    working on classpath, 2

--- maddi-inspection-openjdk/src/test/java/org/e2immu/language/inspection/openjdk/TestJavaInspector2JarOnClasspath.java
+++ maddi-inspection-openjdk/src/test/java/org/e2immu/language/inspection/openjdk/TestJavaInspector2JarOnClasspath.java
@@ -1,30 +1,23 @@
 package org.e2immu.language.inspection.openjdk;
 
-import org.e2immu.language.cst.api.element.SourceSet;
 import org.e2immu.language.cst.api.info.TypeInfo;
 import org.e2immu.language.cst.api.runtime.Runtime;
 import org.e2immu.language.inspection.api.integration.JavaInspector;
 import org.e2immu.language.inspection.api.parser.ParseResult;
 import org.e2immu.language.inspection.api.resource.InputConfiguration;
 import org.e2immu.language.inspection.resource.InputConfigurationImpl;
-import org.e2immu.language.inspection.resource.SourceSetImpl;
 import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import static org.e2immu.language.inspection.openjdk.JavaInspectorImpl.JAR_WITH_PATH_PREFIX;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TestJavaInspector3 {
+public class TestJavaInspector2JarOnClasspath {
 
     private JavaInspector javaInspector;
     private Runtime runtime;
@@ -32,26 +25,12 @@ public class TestJavaInspector3 {
     @BeforeEach
     public void test() throws IOException {
         javaInspector = new JavaInspectorImpl();
-
-        Path commonsCliJar = Path.of("testLib/commons-cli-1.11.0.jar");
-        assertTrue(Files.isReadable(commonsCliJar));
-        SourceSet commonsCli = new SourceSetImpl("commons-cli-1.11.0.jar", List.of(), commonsCliJar.toUri(),
-                StandardCharsets.UTF_8, false, true, true, false, false,
-                Set.of(), Set.of());
-
-        Path maddiSupportClasses = Path.of("../maddi-support/build/classes/java/main/");
-        assertTrue(Files.isDirectory(maddiSupportClasses));
-        SourceSet maddiSupport = new SourceSetImpl("maddi-support", List.of(), maddiSupportClasses.toUri(),
-                StandardCharsets.UTF_8, false, true, true, false, false,
-                Set.of(), Set.of());
-
         InputConfiguration inputConfiguration = new InputConfigurationImpl.Builder()
                 .addSourceSets(JavaInspectorImpl.TEST_PROTOCOL_SOURCE_SET)
                 .addClassPath(InputConfigurationImpl.DEFAULT_MODULES)
+                .addClassPath(JAR_WITH_PATH_PREFIX + "maddi-support-0.8.2.jar")
                 .addClassPath(JAR_WITH_PATH_PREFIX + "slf4j-api-2.0.17.jar")
-                .addClassPathParts(commonsCli, maddiSupport)
                 .build();
-        assertEquals(10, inputConfiguration.classPathParts().size());
         javaInspector.initialize(inputConfiguration);
         runtime = javaInspector.runtime();
     }
@@ -59,10 +38,10 @@ public class TestJavaInspector3 {
     @Language("java")
     public static final String INPUT1 = """
             package a.b;
-            import org.apache.commons.cli.Option;
             import org.e2immu.annotation.ImmutableContainer;
             import org.slf4j.Logger;
             import org.slf4j.LoggerFactory;
+            import org.apache.commons.cli.Util;
             
             @ImmutableContainer
             record C(int k) {
@@ -72,18 +51,13 @@ public class TestJavaInspector3 {
                     LOGGER.info("Returning k*k = {}", k*k);
                     return k*k;
                 }
-            
-                Option makeOption() {
-                    return new Option("-v", "return "+k);
-                }
             }
             """;
 
     @Test
     public void test1() {
-        JavaInspector.ParseOptions options = new JavaInspectorImpl.ParseOptionsBuilder()
-                .setFailFast(true).setDetailedSources(true).build();
-        ParseResult parseResult = javaInspector.parse(Map.of("a.b.C", INPUT1), options).parseResult();
+        ParseResult parseResult = javaInspector.parse(Map.of("a.b.C", INPUT1),
+                JavaInspectorImpl.DETAILED_SOURCES).parseResult();
         TypeInfo C = parseResult.findType("a.b.C");
         assertEquals("@ImmutableContainer", C.annotations().getFirst().toString());
         TypeInfo immutableContainer = runtime.getFullyQualified("org.e2immu.annotation.ImmutableContainer",
---
+++ maddi-inspection-openjdk/src/test/java/org/e2immu/language/inspection/openjdk/TestJavaInspector3RealClasspath.java
@@ -0,0 +1,126 @@
+package org.e2immu.language.inspection.openjdk;
+
+import org.e2immu.language.cst.api.element.SourceSet;
+import org.e2immu.language.cst.api.info.TypeInfo;
+import org.e2immu.language.cst.api.runtime.Runtime;
+import org.e2immu.language.inspection.api.integration.JavaInspector;
+import org.e2immu.language.inspection.api.parser.ParseResult;
+import org.e2immu.language.inspection.api.resource.InputConfiguration;
+import org.e2immu.language.inspection.resource.InputConfigurationImpl;
+import org.e2immu.language.inspection.resource.SourceSetImpl;
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestJavaInspector3RealClasspath {
+
+    public static final String COMMONS_CLI_JAR = "commons-cli-1.11.0.jar";
+    public static final String SLF4J_API_JAR = "slf4j-api-2.0.17.jar";
+
+    private JavaInspector javaInspector;
+    private Runtime runtime;
+
+    @BeforeEach
+    public void test() throws IOException, URISyntaxException {
+        javaInspector = new JavaInspectorImpl();
+
+        Path commonsCliJar = Path.of("testLib/" + COMMONS_CLI_JAR);
+        assertTrue(Files.isReadable(commonsCliJar));
+        SourceSet commonsCli = new SourceSetImpl(COMMONS_CLI_JAR, List.of(), commonsCliJar.toUri(),
+                StandardCharsets.UTF_8, false, true, true, false, false,
+                Set.of(), Set.of());
+
+        Path maddiSupportClasses = Path.of("../maddi-support/build/classes/java/main/");
+        assertTrue(Files.isDirectory(maddiSupportClasses));
+        SourceSet maddiSupport = new SourceSetImpl("maddi-support", List.of(), maddiSupportClasses.toUri(),
+                StandardCharsets.UTF_8, false, true, true, false, false,
+                Set.of(), Set.of());
+
+        URL slf4jJar = findJarInClassPath("org/slf4j/event");
+        SourceSet slf4j =  new SourceSetImpl(SLF4J_API_JAR, List.of(), slf4jJar.toURI(),
+                StandardCharsets.UTF_8, false, true, true, false, false,
+                Set.of(), Set.of());
+
+        InputConfiguration inputConfiguration = new InputConfigurationImpl.Builder()
+                .addSourceSets(JavaInspectorImpl.TEST_PROTOCOL_SOURCE_SET)
+                .addClassPath(InputConfigurationImpl.DEFAULT_MODULES)
+                .addClassPathParts(commonsCli, maddiSupport, slf4j)
+                .build();
+        assertEquals(10, inputConfiguration.classPathParts().size());
+        javaInspector.initialize(inputConfiguration);
+        runtime = javaInspector.runtime();
+    }
+
+    @Language("java")
+    public static final String INPUT1 = """
+            package a.b;
+            import org.apache.commons.cli.Option;
+            import org.e2immu.annotation.ImmutableContainer;
+            import org.slf4j.Logger;
+            import org.slf4j.LoggerFactory;
+            
+            @ImmutableContainer
+            record C(int k) {
+                private static final Logger LOGGER = LoggerFactory.getLogger(C.class);
+            
+                int kSquared() {
+                    LOGGER.info("Returning k*k = {}", k*k);
+                    return k*k;
+                }
+            
+                Option makeOption() {
+                    return new Option("-v", "return "+k);
+                }
+            }
+            """;
+
+    @Test
+    public void test1() {
+        JavaInspector.ParseOptions options = new JavaInspectorImpl.ParseOptionsBuilder()
+                .setFailFast(true).setDetailedSources(true).build();
+        ParseResult parseResult = javaInspector.parse(Map.of("a.b.C", INPUT1), options).parseResult();
+        TypeInfo C = parseResult.findType("a.b.C");
+        assertEquals("@ImmutableContainer", C.annotations().getFirst().toString());
+
+        TypeInfo option = C.findUniqueMethod("makeOption", 0).returnType().typeInfo();
+        assertEquals(COMMONS_CLI_JAR, option.compilationUnit().sourceSet().name());
+
+        TypeInfo logger = C.getFieldByName("LOGGER", true).type().typeInfo();
+        assertEquals(SLF4J_API_JAR, logger.compilationUnit().sourceSet().name());
+
+        TypeInfo immutableContainer = runtime.getFullyQualified("org.e2immu.annotation.ImmutableContainer",
+                true);
+        assertTrue(immutableContainer.typeNature().isAnnotation());
+        assertEquals("maddi-support", immutableContainer.compilationUnit().sourceSet().name());
+    }
+
+    public URL findJarInClassPath(String prefix) throws IOException {
+        Enumeration<URL> roots = getClass().getClassLoader().getResources(prefix);
+        if (roots.hasMoreElements()) {
+            URL url = roots.nextElement();
+            String urlString = url.toString();
+            int bangSlash = urlString.indexOf("!/");
+            if (bangSlash < 0) {
+                throw new UnsupportedEncodingException("? expect !/ in " + urlString);
+            }
+            String strippedUrlString = urlString.substring("jar:".length(), bangSlash);
+            return new URL(strippedUrlString);
+        }
+        return null;
+    }
+}
