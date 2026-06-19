775f6b105b509683aee3fdf7983fd70d3bd7008e
Bart Naudts <Bart.Naudts@codelaser.io>
2026-06-19 12:17:56 +0200

    new test for bug while parsing langchain4j: TestSelfReferentialBound

--- maddi-run-openjdk/src/test/java/org/e2immu/analyzer/run/openjdkmain/TestRunAnalyzer.java
+++ maddi-run-openjdk/src/test/java/org/e2immu/analyzer/run/openjdkmain/TestRunAnalyzer.java
@@ -16,6 +16,7 @@ package org.e2immu.analyzer.run.openjdkmain;
 
 import ch.qos.logback.classic.Level;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +27,7 @@ import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 public class TestRunAnalyzer {
 
     @BeforeAll
