0057f088adc45a6c59b1f8def485d13be956b683
Bart Naudts <Bart.Naudts@codelaser.io>
2026-06-26 07:47:16 +0200

    more basic tests for IsolateMethod

--- maddi-modification-common/src/test/java/org/e2immu/analyzer/modification/common/TestIsolateMethod1.java
+++ maddi-modification-common/src/test/java/org/e2immu/analyzer/modification/common/TestIsolateMethod1.java
@@ -32,7 +32,7 @@ public class TestIsolateMethod1 {
     @BeforeEach
     public void beforeEach() throws IOException {
         javaInspector = javaInspectorFactory().withSources(SourceSetImpl.testProtocolSourceSet());
-        isolateMethod = new IsolateMethod(javaInspector);
+        isolateMethod = new IsolateMethod(javaInspector, "");
     }
 
     @Language("java")
@@ -62,6 +62,78 @@ public class TestIsolateMethod1 {
                 import java.util.ArrayList;
                 import java.util.List;
                 public class X_method { }
+                """;
+        assertEquals(expected, isolateMethod.print(r));
+    }
+
+
+    @Language("java")
+    public static final String INPUT2 = """
+            package a.b;
+            import java.io.IOException;
+            import java.util.ArrayList;
+            import java.util.Arrays;
+            import java.util.List;
+            public class X {
+                public List<String> method(List<String> input) {
+                    if(length(input) < 2) {
+                        List<String> newList = new ArrayList<>();
+                        add(newList, "2", "3");
+                        return newList;
+                    }
+                    return input;
+                }
+                static int length(List<String> list) { return list.size(); }
+                void add(List<String> list, String... elements) {
+                    Arrays.stream(elements).forEach(e -> list.add(e.toLowerCase()));
+                }
+            }
+            """;
+
+    @DisplayName("local method, import")
+    @Test
+    public void test2() {
+        TypeInfo X = javaInspector.parse("a.b.X", INPUT2);
+        IsolateMethod.Result r = isolateMethod.isolate(X.findUniqueMethod("method", 1));
+
+        // import should not contain Arrays (only used outside 'method()'), IOException (not used)
+        @Language("java")
+        String expected = """
+                import java.util.ArrayList;
+                import java.util.List;
+                public class X_method {
+                    static int length(List<String> list) { return 0; }
+                    void add(List<String> list, String ... elements) { }
+                }
+                """;
+        assertEquals(expected, isolateMethod.print(r));
+    }
+
+    @Language("java")
+    public static final String INPUT3 = """
+            package a.b;
+            import org.slf4j.Logger;
+            import org.slf4j.LoggerFactory;
+            import java.util.List;
+            public class X {
+                private static final Logger LOGGER = LoggerFactory.getLogger("X");
+                record S(String s) { }
+                record R(int i, List<S> strings) { }
+                void method(R r) {
+                    LOGGER.info("... {}", r);
+                }
+            }
+            """;
+
+    @DisplayName("local types, static field")
+    @Test
+    public void test3() {
+        TypeInfo X = javaInspector.parse("a.b.X", INPUT3);
+        IsolateMethod.Result r = isolateMethod.isolate(X.findUniqueMethod("method", 1));
+
+        @Language("java")
+        String expected = """
+                public class X_method { Logger LOGGER; class Logger {void info(String arg0, Object arg1) { } } class R { } }
                 """;
         assertEquals(expected, isolateMethod.print(r));
     }
